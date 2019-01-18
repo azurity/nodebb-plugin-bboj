4faaa2febe0017b0b15c9da4f2e68b9f79b623b6
azurity <784227594@qq.com>
2019-01-18 21:07:24 +0800

    init

---
+++ lib.js
@@ -0,0 +1,32 @@
+const Group = require.main.require('./src/groups')
+
+const bbojInterface = {
+    parsePost(data, callback) {
+        Group.get('problem-makers', { uid: data.postData.uid }, (err, result) => {
+            data.postData.content = applyOJ(data.postData.content, !err && !!result && result.isMember)
+            callback(null, data)
+        })
+    }
+}
+
+function applyOJ(data, type) {
+    if (type) {
+        return data.replace(/!&lt;@([0-9a-f]+)&gt;/g, `<div id="$1" class="well"></div>
+        <script>
+        fetch(\`http://localhost:3000/view?uid=\${window.app.user.uid}&problem=$1\`,
+            {
+                method:'get',
+                mode: 'cors'
+            })
+            .then(function (res) {
+                return res.text()
+            }).then((text) => {
+                document.getElementById('$1').innerHTML = text
+            })
+        </script>`)
+    } else {
+        return data.replace(/!&lt;@[0-9a-f]+&gt;/g, '<div class="well">当前题目无法显示</div>')
+    }
+}
+
+module.exports = bbojInterface
